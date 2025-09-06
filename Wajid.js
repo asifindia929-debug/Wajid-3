c1f2cba97b96023b4e2455df26f590cdaef4b96e
asifindia929-debug <asifindia929@gmail.com>
2025-09-06 17:47:44 +0530

    Add files via upload

---
+++ Wajid.js
@@ -0,0 +1,9 @@
+function funnyMessage() {
+  const messages = [
+    "Arre bhootni, ab maan bhi ja 😂",
+    "Forgive me? Matlab chhodo na yaar 😜",
+    "Nakhre mat dikhana 🤡",
+    "Tu gussa karegi toh ro dunga 😭"
+  ];
+  alert(messages[Math.floor(Math.random() * messages.length)]);
+}
